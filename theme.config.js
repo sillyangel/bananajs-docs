c170ff0610e7a77f9e5dfaac3dce1369a5b8fa20
sillyangel <sillyangel3@gmail.com>
2022-04-30 15:53:12 -0500

    big edit

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/sillyangel/banananjs-docs',
+  docsRepositoryBase: 'https://github.com/sillyangel/banananjs-docs/blob/master',
+  titleSuffix: ' – Bananajs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Bananajs</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        The Offical banananjs docs
       </span>
     </>
   ),
@@ -16,45 +16,45 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="@sillyangel/bananajs docs" />
+      <meta name="og:description" content="@sillyangel/bananajs docs" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="https://banana-corp.tk/icons/icon.png" />
+      <meta name="twitter:site:domain" content="docs.banana-corp.tk" />
+      <meta name="twitter:url" content="https://docs.banana-corp.tk" />
+      <meta name="og:title" content="@sillyangel/bananajs docs" />
+      <meta name="og:image" content="https://banana-corp.tk/icons/icon.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
+        href="https://banana-corp.tk/icons/icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="https://banana-corp.tk/icons/icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="https://banana-corp.tk/icons/icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/favicon-96x96.png"
+        href="https://banana-corp.tk/icons/icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="https://banana-corp.tk/icons/icon.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="https://banana-corp.tk/icons/icon.png" />
     </>
   ),
   search: true,
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © sillyangel.</>,
   unstable_faviconGlyph: '👋',
 }
